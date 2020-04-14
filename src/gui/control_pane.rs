2024a358306ca5a2b6ce4c83c8806d578fe3f9c7
Jim Posen <jim.posen@gmail.com>
2020-04-14 14:24:25 -0700

    ControlPane has a TreeView of ports. Not populated.

--- src/gui/control_pane.rs
+++ src/gui/control_pane.rs
@@ -1,4 +1,5 @@
-use gtk::Orientation;
+use glib::Type;
+use gtk::{Orientation, TreeSelection};
 use gtk::prelude::*;
 
 const STYLE: &[u8] = include_bytes!("control_pane.css");
@@ -13,5 +14,28 @@ pub fn new() -> Result<gtk::Box, glib::Error> {
 	style_ctx.add_provider(&style_provider, 1);
 	style_ctx.add_class("control-pane");
 
+	let column_types = [Type::String];
+
+	let list_store = gtk::ListStore::new(&column_types[..]);
+
+	let renderer = gtk::CellRendererText::new();
+	let column = gtk::TreeViewColumn::new();
+	column.pack_start(&renderer, true);
+	column.set_title("Port");
+	column.add_attribute(&renderer, "text", 0);
+
+	let list_view = gtk::TreeView::new_with_model(&list_store);
+	list_view.append_column(&column);
+	list_view.show();
+
+	pane.add(&list_view);
+
+	let selection = list_view.get_selection();
+	selection.connect_changed(on_changed);
+
 	Ok(pane)
+}
+
+fn on_changed(selection: &TreeSelection) {
+
 }
